22dc4e89b6fe4faee3700d74d04cd206eb30fa0a
Yifan Wang <cheetahyf@gmail.com>
2021-05-13 11:48:37 -0700

    Adde basic time management: 1/20 of remaining time + increment

--- drawfish.py
+++ drawfish.py
@@ -47,7 +47,24 @@ def parse_uci(board: chess.Board, msg: str):
         print(board.result())
 
     if msg[0:2] == "go":
-        move = choose_move(board)
+        tl_str = "wtime" if board.turn == chess.WHITE else "btime"
+        inc_str = "winc" if board.turn == chess.WHITE else "binc"
+        split = msg.split()
+        time_left = None
+        increment = None
+        if tl_str in split:
+            try:
+                time_left = int(split[split.index(tl_str) + 1])
+            except IndexError:
+                time_left = None
+        if inc_str in split:
+            try:
+                increment = int(split[split.index(inc_str) + 1])
+            except IndexError:
+                increment = None
+
+        move = choose_move(board, time_left, increment)
+
         print("bestmove %s" % move.uci())
 
 def get_move(board):
@@ -60,13 +77,22 @@ def get_move(board):
 def render(board):
     print(board)
 
-def choose_move(board):
+def manage_time(time_left, increment, num_legal_moves) -> chess.engine.Limit:
+    if increment == None:
+        increment = 0
+    total = (increment + 0.05 * time_left)/1000
+    return chess.engine.Limit(time = total/num_legal_moves)
+    
+
+def choose_move(board, time_left = None, increment = None):
     chosen = chess.Move.null
     chosen_score = 100001
+    print(time_left, increment)
+    limit = chess.engine.Limit(time = 0.1) if time_left == None else manage_time(time_left, increment, len(list(board.legal_moves)))
+    print(limit)
     for move in board.legal_moves:
         board.push(move)
-        #1 million nodes
-        info = engine.analyse(board, chess.engine.Limit(time = 0.2))
+        info = engine.analyse(board, limit)
         score = abs(info["score"].relative.score(mate_score = 100000))
         if score < chosen_score:
             chosen = move
